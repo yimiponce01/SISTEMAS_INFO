d1548d3d9d73323cb2ccd4ff9b3da593aaa02073
Sofia-Itusaca <126581942+Sofia-Itusaca@users.noreply.github.com>
2026-05-20 09:39:36 -0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
         return path.resolve(__dirname, 'src/assets', filename)
@@ -17,20 +16,31 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/SISTEMAS_INFO/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+
     react(),
+
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  server: {
+    host: '0.0.0.0',
+    port: 3409,
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 3409,
+  },
+
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
